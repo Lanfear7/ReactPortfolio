221feff84a20586574b4f47fc282ac727c9b1dc4
Brennan <brennan1086@gmail.com>
2022-06-11 22:25:24 -0700

    Updated Project2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,12 +7,21 @@ import Project1002 from '../public/images/project1-002.png'
 import Project1003 from '../public/images/project1-003.png'
 import Project1004 from '../public/images/project1-004.png'
 
+import Project2001 from '../public/images/project2-001.png'
+import Project2002 from '../public/images/project2-002.png'
+import Project2003 from '../public/images/project2-003.png'
+import Project2004 from '../public/images/project2-004.png'
+
 
 import ReactLogo from '../public/images/ReactLogo.png'
-import Express from '../public/images/express.png'
 import Lambda from '../public/images/lambdaLogo.png'
 import AWS from '../public/images/aws.png'
 import DynamoDB from '../public/images/dynamoDB.png'
+import MySql from '../public/images/mysql.png'
+import Node from '../public/images/node.png'
+import Express from '../public/images/express.png'
+import Spring from '../public/images/spring.png'
+import EmailJs from '../public/images/emailjs.png'
 
 
 
@@ -88,21 +97,21 @@ function Projects() {
               <h1>RuneDen</h1>
               <p>RuneDen is a hub for gamers to get texture packs or seeds for their Minecraft worlds! I bild this app using React,Node/Express, and MySQL. You are able to create a account and login inorder to get varified and post texture packs. You can download any texture pack avalibe without an account though.</p>
               <div className='Links'>
-                <a>Live Link</a>
+                
               </div>
             </div>
-            <div className='TeachStackLogos'>
+            <div className='TeachStackLogos Light'>
               <figure>
                 <img src={ReactLogo}></img>
               </figure>
               <figure>
-                <img src={AWS}></img>
+                <img src={Node}></img>
               </figure>
               <figure>
-                <img src={Lambda}></img>
+                <img src={Express}></img>
               </figure>
-              <figure className='dynamo'>
-                <img src={DynamoDB}></img>
+              <figure>
+                <img src={MySql}></img>
               </figure>
             </div>
           </div>
@@ -127,38 +136,34 @@ function Projects() {
           <div className='Project'>
           <div className='AnimationContainer'>
             <animated.figure style={styles1} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1003}></img>
+              <img src={Project2003}></img>
             </animated.figure>
             <animated.figure style={styles2} className="pic1" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1001}></img>
+              <img src={Project2001}></img>
             </animated.figure>
             <animated.figure style={styles3} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1002}></img>
+              <img src={Project2002}></img>
             </animated.figure>
             <animated.figure style={styles4} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1004}></img>
+              <img src={Project2004}></img>
             </animated.figure>
           </div>
           <div className='ProjectContents'>
             <div className='MainContent'>
-              <h1>Project 2</h1>
-              <p>RuneDen is a hub for gamers to get texture packs or seeds for their Minecraft worlds! I bild this app using React,Node/Express, and MySQL. You are able to create a account and login inorder to get varified and post texture packs. You can download any texture pack avalibe without an account though.</p>
+              <h1>Portfolio</h1>
+              <p>This portfolio was built useing React along with some React packages such as React-Spring for the animation and emailJs for a potential client to beable to contact me.</p>
               <div className='Links'>
-                <a>Live Link</a>
               </div>
             </div>
-            <div className='TeachStackLogos'>
+            <div className='TeachStackLogos Light'>
               <figure>
                 <img src={ReactLogo}></img>
               </figure>
               <figure>
-                <img src={AWS}></img>
+                <img src={Spring}></img>
               </figure>
               <figure>
-                <img src={Lambda}></img>
-              </figure>
-              <figure className='dynamo'>
-                <img src={DynamoDB}></img>
+                <img src={EmailJs}></img>
               </figure>
             </div>
           </div>
@@ -167,39 +172,35 @@ function Projects() {
          <div className='Project'>
           <div className='ProjectContents'>
             <div className='MainContent'>
-              <h1>Project 2</h1>
-              <p>RuneDen is a hub for gamers to get texture packs or seeds for their Minecraft worlds! I bild this app using React,Node/Express, and MySQL. You are able to create a account and login inorder to get varified and post texture packs. You can download any texture pack avalibe without an account though.</p>
+              <h1>Portfolio</h1>
+              <p>This portfolio was built useing React along with some React packages such as React-Spring for the animation and emailJs for a potential client to beable to contact me.</p>
               <div className='Links'>
-                <a>Live Link</a>
               </div>
             </div>
-            <div className='TeachStackLogos'>
+            <div className='TeachStackLogos Light'>
               <figure>
                 <img src={ReactLogo}></img>
               </figure>
               <figure>
-                <img src={AWS}></img>
+                <img src={Spring}></img>
               </figure>
               <figure>
-                <img src={Lambda}></img>
-              </figure>
-              <figure className='dynamo'>
-                <img src={DynamoDB}></img>
+                <img src={EmailJs}></img>
               </figure>
             </div>
           </div>
           <div className='AnimationContainer'>
             <animated.figure style={styles1} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1003}></img>
+              <img src={Project2003}></img>
             </animated.figure>
             <animated.figure style={styles2} className="pic1" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1001}></img>
+              <img src={Project2001}></img>
             </animated.figure>
             <animated.figure style={styles3} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1002}></img>
+              <img src={Project2002}></img>
             </animated.figure>
             <animated.figure style={styles4} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1004}></img>
+              <img src={Project2004}></img>
             </animated.figure>
           </div>
         </div>
@@ -210,7 +211,6 @@ function Projects() {
               <h1>Project 3</h1>
               <p>RuneDen is a hub for gamers to get texture packs or seeds for their Minecraft worlds! I bild this app using React,Node/Express, and MySQL. You are able to create a account and login inorder to get varified and post texture packs. You can download any texture pack avalibe without an account though.</p>
               <div className='Links'>
-                <a>Live Link</a>
               </div>
             </div>
             <div className='TeachStackLogos TeachStackLogosWhite'>
