9b90f02578121d83f3ec930182928199b50e6e3a
Brennan <brennan1086@gmail.com>
2022-06-11 23:27:10 -0700

    Project 3 updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,16 +12,21 @@ import Project2002 from '../public/images/project2-002.png'
 import Project2003 from '../public/images/project2-003.png'
 import Project2004 from '../public/images/project2-004.png'
 
+import Project3001 from '../public/images/project3-001.png'
+import Project3002 from '../public/images/project3-002.png'
+import Project3003 from '../public/images/project3-003.png'
+import Project3004 from '../public/images/project3-004.png'
+
 
 import ReactLogo from '../public/images/ReactLogo.png'
-import Lambda from '../public/images/lambdaLogo.png'
-import AWS from '../public/images/aws.png'
-import DynamoDB from '../public/images/dynamoDB.png'
+import JSlogo from '../public/images/JSlogo.png'
 import MySql from '../public/images/mysql.png'
 import Node from '../public/images/node.png'
 import Express from '../public/images/express.png'
 import Spring from '../public/images/spring.png'
 import EmailJs from '../public/images/emailjs.png'
+import HTML from '../public/images/html.png'
+import CSS from '../public/images/css.png'
 
 
 
@@ -208,38 +213,35 @@ function Projects() {
         <div className='Project'>
           <div className='ProjectContents'>
             <div className='MainContent MainContentWhite'>
-              <h1>Project 3</h1>
-              <p>RuneDen is a hub for gamers to get texture packs or seeds for their Minecraft worlds! I bild this app using React,Node/Express, and MySQL. You are able to create a account and login inorder to get varified and post texture packs. You can download any texture pack avalibe without an account though.</p>
+              <h1>Forever Hungry</h1>
+              <p>Forever Hungry is a recipe app where you can find and search for the pefect recipe for tonights dinner. This app is built using HTML/CSS and vanilla JavaScript, to get the recipes im using the Spoonacular API then displaying those tasty treats in a user friendly way.</p>
               <div className='Links'>
               </div>
             </div>
-            <div className='TeachStackLogos TeachStackLogosWhite'>
-              <figure>
-                <img src={ReactLogo} className="React" ></img>
-              </figure>
-              <figure>
-                <img src={AWS} className="AWS"></img>
-              </figure>
-              <figure>
-                <img src={Lambda}></img>
+            <div className='TeachStackLogos'>
+              <figure className='dynamo'>
+                <img src={HTML}></img>
               </figure>
               <figure className='dynamo'>
-                <img src={DynamoDB}></img>
+                <img src={CSS}></img>
+              </figure>
+              <figure className='dynamo'>
+                <img src={JSlogo}></img>
               </figure>
             </div>
           </div>
           <div className='AnimationContainer'>
             <animated.figure style={styles1} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1003}></img>
+              <img src={Project3003}></img>
             </animated.figure>
             <animated.figure style={styles2} className="pic1" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1001}></img>
+              <img src={Project3001}></img>
             </animated.figure>
             <animated.figure style={styles3} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1002}></img>
+              <img src={Project3002}></img>
             </animated.figure>
             <animated.figure style={styles4} className="pic2" onClick={(e)=>{ImgPopUpSetUp(e)}}>
-              <img src={Project1004}></img>
+              <img src={Project3004}></img>
             </animated.figure>
           </div>
         </div>
